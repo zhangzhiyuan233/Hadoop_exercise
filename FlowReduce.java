7685c475f0b257a9c26e36b5c80f2ddb02993578
zhangzhiyuan233 <zhangzhiyuan1020@gmail.com>
2021-07-18 21:08:26 +0800

    update homework1

---
+++ FlowReduce.java
@@ -0,0 +1,19 @@
+package com.atguigu.mapreduce.flow;
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+public class FlowReducer extends Reducer<Text,FlowBean,Text,FlowBean>{
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values,Context context)
+            throws IOException, InterruptedException {
+        // 计算总的流量
+        long sum_upFlow = 0;
+        long sum_downFlow = 0;
+        for(FlowBean bean : values) {
+            sum_upFlow += bean.getUpFlow();
+            sum_downFlow += bean.getDownFlow();
+        }
+        //输出
+        context.write(key,new FlowBean(sum_upFlow,sum_downFlow));
+    }
+}
